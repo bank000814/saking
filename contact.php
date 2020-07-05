1ace86cc509ec4c10fbcdbf6f0bc12476b759a59
Nantarwath komto <nantarwathkomto@Nantarwaths-MacBook-Pro.local>
2020-07-05 13:34:32 +0700

    start

---
+++ contact.php
@@ -0,0 +1,81 @@
+
+	
+	<?php require_once('_header.php'); ?>
+
+<section class="banner">
+	<div class="container mini">
+		<div class="row pt-3">
+			<div class="col-12 col-sm-6 order-1 order-sm-0">
+				<img class="img-fluid img-wow" src="./img/bn-wow.png" alt="">
+			</div>
+			<div class="col-12 col-sm-6 order-0 order-sm-1 text-center">
+				<img class="img-fluid mb-2" src="./img/bn-1.png" alt="">
+				<img class="img-fluid mb-2" src="./img/bn-2.png" alt="">
+				<img class="img-fluid mb-2" src="./img/bn-3.png" alt="">
+				<a href="#">
+					<img class="img-fluid mb-2" src="./img/bn-line.png" alt="">
+				</a>
+				<a href="#">
+					<img class="img-fluid mb-2" src="./img/bn-start.png" alt="">
+				</a>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="contact">
+	
+	<div class="st-3">
+		<div class="container mini">
+			<div class="row">
+				<div class="col-12 col-sm-8">
+					<div class="card my-3">
+						<div class="card-header"><h4>ติดต่อเรา</h4></div>
+						<div class="card-body bg text-center">
+							<div class="contact-label pb-3">
+								<p class="c-white">
+									ติดต่อสำนักงาน ท่านสามารถ ติดต่อสอบถามทางทีมงานได้ทุกเรื่อง
+									ที่เกี่ยวกับ คาสิโนออนไลน์และ กีฬาออนไลน์ ไม่ว่าจะเติมเงิน ถอนเงิน โยกเงิน เช็คยอดเงิน
+								</p>
+								<h4 class="c-red">Call Center 24 ชั่วโมง</h4>
+								<h5>
+									<a class="c-yellow" href="tel:089999789">089-999-789</a>
+									<small class="">|</small>
+									<span class="c-white">LINE ID :</span>
+									<a class="c-green" href="#">@mbk789</a>
+								</h5>
+							</div>
+							<div class="qr">
+								<img class="img-fluid" src="./img/qrcode.png" alt="">
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-12 col-sm-4">
+					<div class="card my-3" style="margin-bottom:2rem!important">
+						<div class="card-body p-0">
+							<img src="./img/home/380x343.png" class="img-fluid" alt="">
+						</div>
+					</div>
+					<div class="card my-3">
+						<div class="card-body p-0">
+							<img src="./img/home/380x243.png" class="img-fluid" alt="">
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-12 col-sm-8">
+					<div class="card my-3">
+						<div class="card-body p-0">
+							<img src="./img/home/600x117.png" class="img-fluid" alt="">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+</section>
+
+<?php require_once('_footer.php'); ?>
---
+++ promotion.php
@@ -0,0 +1,122 @@
+
+	
+	<?php require_once('_header.php'); ?>
+
+<section class="banner">
+	<div class="container mini">
+		<div class="row pt-3">
+			<div class="col-12 col-sm-6 order-1 order-sm-0">
+				<img class="img-fluid img-wow" src="./img/bn-wow.png" alt="">
+			</div>
+			<div class="col-12 col-sm-6 order-0 order-sm-1 text-center">
+				<img class="img-fluid mb-2" src="./img/bn-1.png" alt="">
+				<img class="img-fluid mb-2" src="./img/bn-2.png" alt="">
+				<img class="img-fluid mb-2" src="./img/bn-3.png" alt="">
+				<a href="#">
+					<img class="img-fluid mb-2" src="./img/bn-line.png" alt="">
+				</a>
+				<a href="#">
+					<img class="img-fluid mb-2" src="./img/bn-start.png" alt="">
+				</a>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="promotion">
+	
+	<div class="st-3">
+		<div class="container mini">
+			<div class="row">
+				<div class="col-12 col-sm-8">
+					<div class="card my-3">
+						<div class="card-header"><h4>โปรโมชั่น mbk789.com</h4></div>
+						<div class="card-body">
+							<div class="row">
+								<div class="col-12 mb-3">
+									<a href="#">
+										<div class="card item">
+											<img src="./img/promotion/1-560x210.png" class="card-img-top" alt="">
+											<p class="card-text p-2">โปรโมชั่น 10% สมาชิกใหม่และเก่า รับรายวัน แจกสูงสุด 1000</p>
+											<p class="card-text px-2">ข้อกำหนดและเงื่อนไข :</p>
+											<ul class="c-white">
+												<li>กดปุ่มรับโบนัสได้เอง อัตโนมัติ</li>
+												<li>รับสูงสุด 1,000</li>
+												<li>ถ้ารับโปรนี้ไปแล้ว ของวันแรก สิทธิ์โปรอื่นๆ จะหมดสิทธิ์รับ จะตัดรอบหลังเที่ยงคืนถึงจะสามารถฝากแล้วมีสิทธิ์รับโปรอื่น</li>
+												<li>ทำยอดหมุนเวียน Turnover 20 เท่า จะสามารถถอนได้</li>
+											</ul>
+											<span class="c-yellow">ตัวอย่าง : <span class="c-white">ฝากครั้งแรก 500 บาท รับโบนัส 10% = 50 บาท รวมทั้งหมดเป็น 550 บาท 
+ต้องทำยอดเทิร์น 11,000 บาท (จึงจะสามารถถอนโบนัสได้)</span></span>
+										</div>
+									</a>
+								</div>
+								<div class="col-12 mb-3">
+									<a href="#">
+										<div class="card item">
+											<img src="./img/promotion/2-560x210.png" class="card-img-top" alt="">
+											<p class="card-text p-2">โปรโมชั่น 100% สมาชิกใหม่ แจกสูงสุด 1,000</p>
+											<p class="card-text px-2">ข้อกำหนดและเงื่อนไข :</p>
+											<ul class="c-white">
+												<li>กดปุ่มรับโบนัสได้เอง อัตโนมัติ</li>
+												<li>รับสูงสุด 1,000</li>
+												<li>ถ้ารับโปรนี้ไปแล้ว ของวันแรก สิทธิ์โปรอื่นๆ จะหมดสิทธิ์รับ จะตัดรอบหลังเที่ยงคืนถึงจะสามารถฝากแล้วมีสิทธิ์รับโปรอื่น</li>
+												<li>ทำยอดหมุนเวียน Turnover 30 เท่า จะสามารถถอนได้</li>
+												<li>ห้ามพนันทบยอดเกิน 3 ไม้ หากตรวจเจอจะไม่สามารถถอนเงินได้ทุกกรณี</li>
+											</ul>
+											<span class="c-yellow">ตัวอย่าง : <span class="c-white">ฝากครั้งแรก 1,000 บาท รับโบนัส 100% = 1,000 บาท รวมทั้งหมดเป็น 2,000 บาท
+ต้องทำยอดเทิร์น 60,000 บาท จึงจะสามารถถอนได้ (หากเล่นไม่ครบเทิร์น กรุณาเล่นจนหมด
+ก่อนฝากเข้ามาใหม่ มิฉะนั้นท่านจะมีสถานะรับโบนัสค้างอยู่ค่ะ)</span></span>
+										</div>
+									</a>
+								</div>
+								<div class="col-12 mb-3">
+									<a href="#">
+										<div class="card item">
+											<img src="./img/promotion/3-560x210.png" class="card-img-top" alt="">
+											<p class="card-text p-2">โปรโมชั่นต้อนรับสมาชิกใหม่ รับเครดิตฟรี 200 บาท</p>
+											<p class="card-text px-2">ข้อกำหนดและเงื่อนไข :</p>
+											<ul class="c-white">
+												<li>โปรโมชั่นนี้ให้เฉพาะสมาชิกใหม่เท่านั้น</li>
+												<li>สมาชิกใหม่จะต้องฝากเงิน 200฿ ขึ้นไปถึงจะรับเครดิตฟรีอีก 200฿</li>
+												<li>ลูกค้าจะต้องทำเทิร์น 4เท่า (เพื่อถอนเงิน)</li>
+											</ul>
+											<p class="c-red">*** หมายเหตุ ***</p>
+											<ul class="bl-red c-white">
+												<li>ห้ามเดิมหมดหน้าตักติดต่อกัน 3 ตา</li>
+												<li>หากตรวจเจอจะไม่สามารถถอนเงินได้ทุกกรณี เช่น 400 800 1600 ไม่สนับสนุนพวกล่าโปรนะค่ะ</li>
+											</ul>
+										</div>
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-12 col-sm-4">
+					<div class="card my-3" style="margin-bottom:2rem!important">
+						<div class="card-body p-0">
+							<img src="./img/home/380x343.png" class="img-fluid" alt="">
+						</div>
+					</div>
+					<div class="card my-3">
+						<div class="card-body p-0">
+							<img src="./img/home/380x243.png" class="img-fluid" alt="">
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-12 col-sm-8">
+					<div class="card my-3">
+						<div class="card-body p-0">
+							<img src="./img/home/600x117.png" class="img-fluid" alt="">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+</section>
+
+<?php require_once('_footer.php'); ?>
